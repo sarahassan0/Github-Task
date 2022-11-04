c9373e936dec8f944b85980cbb0752a057ddd262
Sarah hassan <sarraree5@gmail.com>
2022-11-04 15:58:20 +0200

    complete user end-points

--- routes/users.js
+++ routes/users.js
@@ -21,4 +21,32 @@ router.post("/", async function (req, res, next) {
 
 })
 
+
+
+// Delete User by its ID
+router.delete('/:id', async (req, res, next) => {
+    const _id = req.params.id
+    try {
+        let user = await userModel.findByIdAndRemove(_id)
+        res.json(`User with ID ${_id} has been deleted`)
+
+    } catch (err) {
+        res.status(420).json(`Cannot delete User with ID ${_id}.Error: ${err} `)
+    }
+})
+
+
+// get User's To-dos 
+router.get('/:id/todos', async (req, res, next) => {
+    const _id = req.params.id
+    try {
+        let todos = await todoModel.find({ userID: _id })
+        res.json(todos)
+    } catch (err) {
+        res.status(420).json(`Cannot get this User's To-dos. Error: ${err}`)
+    }
+})
+
+
+
 module.exports = router;
